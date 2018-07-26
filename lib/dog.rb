9efb4702f2bb810c5b6a7205d68c875ada74d5e1
Reice Griffin <reicelovesllamas@gmail.com>
2018-07-26 18:34:22 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,13 @@
 # dog.rb
+
+class Dog 
+  
+    def intialize(name)
+      @name=name
+    end
+    
+  end
+  
+  fido = Dog.new("Fido")
+  snoopy = Dog.new("Snoopy")
+  lassie = Doh.new("Lassie")
